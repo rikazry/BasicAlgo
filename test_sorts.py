5867dc67ed4e69380dbfc47bb8051247aa354ba2
random-walk <h4ckgen@gmail.com>
2015-01-02 13:49:03 -0500

    added quicksort to test

--- test_sorts.py
+++ test_sorts.py
@@ -5,7 +5,7 @@ import timeit
 
 class test(unittest.TestCase):
     def setUp(self):
-        self.random_seq_size = 10000
+        self.random_seq_size = 100000 
         self.random_seq = random.sample(range(self.random_seq_size),\
                 self.random_seq_size)
     def test_merge_sort(self):
@@ -14,5 +14,13 @@ class test(unittest.TestCase):
         end = timeit.default_timer()
         print("Merge sort took %s sec" % str(end-start))
         self.assertEqual(Y, sorted(self.random_seq))
+
+    def test_quick_sort(self):
+        start = timeit.default_timer()       
+        Y = sorts.quick_sort(self.random_seq)
+        end = timeit.default_timer()
+        print("Quick sort took %s sec" % str(end-start))
+        self.assertEqual(Y, sorted(self.random_seq))
+
 if __name__ == '__main__':
    unittest.main() 
