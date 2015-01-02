17fee8551b302dcf4ae5e4b8ec28ad276f75b4f1
random-walk <h4ckgen@gmail.com>
2015-01-02 16:22:24 -0500

    added test for quick_selection

--- test_sorts.py
+++ test_sorts.py
@@ -5,7 +5,7 @@ import timeit
 
 class test(unittest.TestCase):
     def setUp(self):
-        self.random_seq_size = 100000 
+        self.random_seq_size = 100 
         self.random_seq = random.sample(range(self.random_seq_size),\
                 self.random_seq_size)
     def test_merge_sort(self):
@@ -22,5 +22,12 @@ class test(unittest.TestCase):
         print("Quick sort took %s sec" % str(end-start))
         self.assertEqual(Y, sorted(self.random_seq))
 
+    def test_quick_select(self):
+        sorted_list = sorted(self.random_seq)
+        for k in range(len(self.random_seq)):
+            self.assertEqual(sorted_list[k],\
+                    sorts.quick_select(self.random_seq,k))
+        
+        
 if __name__ == '__main__':
    unittest.main() 
