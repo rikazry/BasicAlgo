3ee87bca9f6c32f0a1434d8b2e5c9b0fab6669a9
random-walk <h4ckgen@gmail.com>
2015-01-02 16:21:57 -0500

    added quicksort and quickselection

--- sorts.py
+++ sorts.py
@@ -34,3 +34,69 @@ def merge_sort(x):
             return Y
     return Y
 
+def partition(Y,p):
+    '''
+    partitions the list using pivot, p
+    '''
+    left = []
+    right = []
+    pivots = []
+    for k in range(len(Y)):
+        if Y[k] < p:
+            left.append(Y[k])
+        elif Y[k] > p:
+            right.append(Y[k])
+        elif Y[k] == p:
+            pivots.append(Y[k])
+    return left,right,pivots
+
+def quick_sort(X):
+    '''
+    quick sort
+    '''
+    
+    if len(X) <= 1:
+        return X
+
+    pivot = X[0]
+    left,right,pivots =  partition(X,pivot)
+    left_sorted = quick_sort(left)
+    right_sorted = quick_sort(right)
+    left_sorted.extend(pivots)
+    left_sorted.extend(right_sorted)
+    return left_sorted
+
+
+def quick_select(X,n):
+    '''
+    smartly finds n_th element in a list
+    uses quickselection technique
+    '''
+    
+    # base case
+    if len(X) == 1:
+        return X[0]
+
+    pivot = X[0]
+    left,right,pivots = partition(X,pivot)
+   
+    # left is out
+    # n = 0,...,len(X)-1
+    # left:[0,...,len(left)-1] 
+    # pivots: [len(left),..,len(left)+len(pivots)-1]
+    # right: [len(left)+len(pivots),...,len(X)-1] 
+    
+    # check if element is in left block 
+    if len(left) > n:
+        return quick_select(left,n)
+    
+    # check if element is in pivots block
+    elif len(left)+len(pivots) > n:
+        return pivot
+    
+    # if not in left or pivots, must be in right block
+    else:
+        return quick_select(right,n-len(left)-len(pivots))
+
+    
+
